519770205a86d558a819187697b1d2796e57194c
根仔mac0000 <mima0000@MacBook-Air.local>
2023-11-25 21:47:11 +0800

    Initial commit

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,31 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+import '@umijs/max';
+import React from 'react';
+const Footer: React.FC = () => {
+  const defaultMessage = 'gpt对话式数据分析';
+  const currentYear = new Date().getFullYear();
+  return (
+    <DefaultFooter
+      style={{
+        background: 'none',
+      }}
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        {
+          key: '框架',
+          title: 'Antdesign pro',
+          href: 'https://pro.ant.design',
+          blankTarget: true,
+        },
+        {
+          key: 'github',
+          title: <GithubOutlined />,
+          href: 'https://github.com/ant-design/ant-design-pro',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
+export default Footer;
